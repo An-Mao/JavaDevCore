f9ea8ca8f941c8735823ad6dae5e1a5752cf9137
AnMao <lxxl@live.cn>
2025-07-08 08:16:01 +0800

    25.07.0701

--- src/main/java/dev/anye/core/Main.java
+++ src/main/java/dev/anye/core/Main.java
@@ -1,6 +1,11 @@
 package dev.anye.core;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
         /*
          * TODO
--- src/main/java/dev/anye/core/system/_ThreadMonitor.java
+++ src/main/java/dev/anye/core/system/_ThreadMonitor.java
@@ -1,5 +1,8 @@
 package dev.anye.core.system;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.time.LocalTime;
 
 public class _ThreadMonitor {
@@ -7,7 +10,7 @@ public class _ThreadMonitor {
     private Thread thread;
     private final long cooldownMillis;
     private final long s;
-    private final _Log _log = new _Log();
+    private final Logger _log = LoggerFactory.getLogger(_ThreadMonitor.class);
 
     public _ThreadMonitor(Runnable task, long cooldownMillis) {
         this.task = task;
@@ -23,12 +26,12 @@ public class _ThreadMonitor {
     private Thread createThread(Runnable task) {
         Thread newThread = new Thread(task);
         newThread.setUncaughtExceptionHandler((t, e) -> {
-            _log.error("Thread " + t.getName() + " crashed! " + e.getMessage());
+            _log.error("Thread {} crashed! {}", t.getName(), e.getMessage());
             try {
-                _log.info("waiting " + LocalTime.ofSecondOfDay(s) + " seconds...");
+                _log.info("waiting {} seconds...", LocalTime.ofSecondOfDay(s));
                 Thread.sleep(cooldownMillis);
             } catch (InterruptedException ex) {
-                _log.warn("cooldown interrupted: " + ex.getMessage());
+                _log.warn("cooldown interrupted: {}", ex.getMessage());
             }
             restart();
         });
