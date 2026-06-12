797630056403eb83f5faf1f5f0f166c7e80ad0da
An-Mao <lxxl@live.cn>
2026-06-12 17:26:58 +0800

    2.0.4-26061201

--- src/main/java/dev/anye/core/pack/_Pack.java
+++ src/main/java/dev/anye/core/pack/_Pack.java
@@ -1,5 +1,7 @@
 package dev.anye.core.pack;
 
+import dev.anye.core.system._File;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -10,15 +12,15 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 public class _Pack {
-
 	public static void writeFiles(String packPath, String putPath, String suffix, boolean replace, String... names) {
 		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
 		for (String name : names) {
-			if (replace && new File(putPath + name + suffix).exists()) continue;
-			try (InputStream inputStream = classLoader.getResourceAsStream(packPath + name + suffix)) {
+			if (replace && new File(_File.getFilePath(putPath, name + suffix)).exists()) continue;
+			String filePath = _File.getFilePath(putPath, name + suffix);
+			try (InputStream inputStream = classLoader.getResourceAsStream(filePath)) {
 				if (inputStream == null)
-					throw new FileNotFoundException("Resource not found: " + packPath + name + suffix);
-				Path outputPath = Paths.get(putPath + name + suffix);
+					throw new FileNotFoundException("Resource not found: " + filePath);
+				Path outputPath = Paths.get(filePath);
 				Files.copy(inputStream, outputPath, StandardCopyOption.REPLACE_EXISTING);
 			} catch (IOException e) {
 				throw new RuntimeException(e);
