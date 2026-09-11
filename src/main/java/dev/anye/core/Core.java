423dc72a9c9544679ee53da324acaa68eeae9faf
An-Mao <lxxl@live.cn>
2026-09-11 19:19:19 +0800

    26091104

--- src/main/java/dev/anye/core/Core.java
+++ src/main/java/dev/anye/core/Core.java
@@ -1,18 +1,47 @@
 package dev.anye.core;
 
+import dev.anye.core.system.task.TimingWheel;
 
-import dev.anye.core.system.task.HashedTimingWheelOld;
-import dev.anye.core.system.task.$_TimingWheel;
-
+import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.TimeUnit;
 
 public class Core {
 	public static final String VERSION = "2.0.6";
 	//private static final Logger LOGGER = Logger.getLogger(Core.class.getName());
 	public static void main(String[] args) {
-		//run();
+		run();
 	}
 
 	public static void run() {
+		// //普通定时任务
+		// TimingWheel wheel = TimingWheel.builder()
+		// 		.tick(10, TimeUnit.MILLISECONDS)
+		// 		.wheelSize(512)
+		// 		.executor(ForkJoinPool.commonPool())
+		// 		.threadName("Core-Timer")
+		// 		.daemon(true)
+		// 		.build();
+
+		// wheel.schedule(
+		// 		() -> System.out.println("Hello"),
+		// 		1,
+		// 		TimeUnit.SECONDS);
+
+		// //周期任务
+		// wheel.scheduleAtFixedRate(
+		// 		() -> {
+		// 			System.out.println("tick");
+		// 		},
+		// 		0,
+		// 		1,
+		// 		TimeUnit.SECONDS);
+
+		// //取消
+		// TimingWheel.TimerTask task = wheel.schedule(
+		// 		() -> System.out.println("test"),
+		// 		10,
+		// 		TimeUnit.SECONDS);
+
+		// task.cancel();
 	}
 }
